6e6fdead0d987536c86329daabcaf708385e34fc
iannesbitt <ian.nesbitt@gmail.com>
2019-01-09 12:53:53 -0500

    bug fixes

--- shake-UDP-local.py
+++ shake-UDP-local.py
@@ -7,7 +7,8 @@ file = open(hostipF, 'r')
 host = file.read().strip()
 file.close()
 
-RS.openSOCK(host=host, port=port)
+RS.initRSlib(dport=port)
+RS.openSOCK()
 
 while 1:								# loop forever
 	print(RS.getDATA())
--- shake-UDP-remote.py
+++ shake-UDP-remote.py
@@ -1,9 +1,36 @@
 import raspberryShake as RS
+import sys, getopt
 
-host = ""				# when running not on the Shake Pi: blank = localhost
-port = 18005				# Port to bind to
 
-RS.openSOCK(host=host, port=port)
+def run(port):
+	RS.initRSlib(dport=port)
+	RS.openSOCK()
 
-while 1:								# loop forever
-	print(RS.getDATA())
+	while 1:								# loop forever
+		print(RS.getDATA())
+
+
+hlp_txt = '''
+This program prints UDP data sent to a specific local port.
+The following command will print data from port 18001:
+
+python shake-UDP-remote.py -p 18001
+
+Use -h to display this message.
+'''
+
+if __name__ == '__main__':
+	prt = 8888
+	opts, args = getopt.getopt(sys.argv[1:], 'hp:', ['help', 'port='])
+	for o, a in opts:
+		if o in ('-h, --help'):
+			print(hlp_txt)
+			exit(0)
+		try:
+			if o in ('-p', 'port='):
+				prt = int(a)
+		except Exception as e:
+			RS.printM('ERROR: Port value must be integer.')
+			RS.printM('Error details: %s' % e)
+			exit(2)
+	run(prt)

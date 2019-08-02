b6a793bbf291869ff7c842053ec5af9695b477d5
Ian Nesbitt <ian.nesbitt@gmail.com>
2019-08-02 16:02:33 -0500

    removing tmalloc for #1

--- rsh_udp/live_example.py
+++ rsh_udp/live_example.py
@@ -12,41 +12,6 @@ import gc
 plt.ion()
 
 
-####################################################
-####################################################
-import linecache
-import os
-import tracemalloc
-
-def display_top(snapshot, key_type='lineno', limit=10):
-    snapshot = snapshot.filter_traces((
-        tracemalloc.Filter(False, "<frozen importlib._bootstrap>"),
-        tracemalloc.Filter(False, "<unknown>"),
-    ))
-    top_stats = snapshot.statistics(key_type)
-
-    print("Top %s lines" % limit)
-    for index, stat in enumerate(top_stats[:limit], 1):
-        frame = stat.traceback[0]
-        # replace "/path/to/module/file.py" with "module/file.py"
-        filename = os.sep.join(frame.filename.split(os.sep)[-2:])
-        print("#%s: %s:%s: %.1f KiB"
-              % (index, filename, frame.lineno, stat.size / 1024))
-        line = linecache.getline(frame.filename, frame.lineno).strip()
-        if line:
-            print('    %s' % line)
-
-    other = top_stats[limit:]
-    if other:
-        size = sum(stat.size for stat in other)
-        print("%s other: %.1f KiB" % (len(other), size / 1024))
-    total = sum(stat.size for stat in top_stats)
-    print("Total allocated size: %.1f KiB" % (total / 1024))
-
-tracemalloc.start()
-####################################################
-####################################################
-
 '''
 A more complex example program that uses rs2obspy to build a stream, then
 plots the result live until the user interrupts the program using CTRL+C.
@@ -174,7 +139,6 @@ def live_stream(port=8888, sta='Z0000', seconds=30, spectrogram=False):
 					width = fig.get_size_inches()[0]				# get the current figure width (inches)
 					plt.close('all')								# close all matplotlib objects
 					gc.collect()									# clean up garbage
-					plt.ion()
 				if spectrogram:
 					s, fig, ax, lines, mult, sg, per_lap, nfft1, nlap1 = plot_gen(
 							s, figsize=(width,3*num_chans), seconds=seconds, spectrogram=spectrogram
@@ -183,8 +147,6 @@ def live_stream(port=8888, sta='Z0000', seconds=30, spectrogram=False):
 					s, fig, ax, lines, mult = plot_gen(s, figsize=(width,3*num_chans), seconds=seconds)	# regenerate line plot
 				if n > 1:
 					rso.RS.printM('Plot regenerated after %s loops.' % (n))
-					snapshot = tracemalloc.take_snapshot()
-					display_top(snapshot)
 
 			i = 0
 			while i < num_chans*mult*(float(rso.sps)/100):	# way of reducing CPU load while keeping stream up to date

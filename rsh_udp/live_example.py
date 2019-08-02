fa5c62cf8d2f067da3d4f60f115613692dc37b5f
Ian Nesbitt <ian.nesbitt@gmail.com>
2019-08-02 13:48:03 -0500

    memory allocation testing for #1

--- rsh_udp/live_example.py
+++ rsh_udp/live_example.py
@@ -10,6 +10,42 @@ import gc
 
 plt.ion()
 
+
+####################################################
+####################################################
+import linecache
+import os
+import tracemalloc
+
+def display_top(snapshot, key_type='lineno', limit=10):
+    snapshot = snapshot.filter_traces((
+        tracemalloc.Filter(False, "<frozen importlib._bootstrap>"),
+        tracemalloc.Filter(False, "<unknown>"),
+    ))
+    top_stats = snapshot.statistics(key_type)
+
+    print("Top %s lines" % limit)
+    for index, stat in enumerate(top_stats[:limit], 1):
+        frame = stat.traceback[0]
+        # replace "/path/to/module/file.py" with "module/file.py"
+        filename = os.sep.join(frame.filename.split(os.sep)[-2:])
+        print("#%s: %s:%s: %.1f KiB"
+              % (index, filename, frame.lineno, stat.size / 1024))
+        line = linecache.getline(frame.filename, frame.lineno).strip()
+        if line:
+            print('    %s' % line)
+
+    other = top_stats[limit:]
+    if other:
+        size = sum(stat.size for stat in other)
+        print("%s other: %.1f KiB" % (len(other), size / 1024))
+    total = sum(stat.size for stat in top_stats)
+    print("Total allocated size: %.1f KiB" % (total / 1024))
+
+tracemalloc.start()
+####################################################
+####################################################
+
 '''
 A more complex example program that uses rs2obspy to build a stream, then
 plots the result live until the user interrupts the program using CTRL+C.
@@ -142,6 +178,8 @@ def live_stream(port=8888, sta='Z0000', seconds=30, spectrogram=False):
 					s, fig, ax, lines, mult = plot_gen(s, figsize=(width,3*len(rso.channels)), seconds=seconds)	# regenerate line plot
 				if n > 1:
 					rso.RS.printM('Plot regenerated after %s loops.' % (n))
+					snapshot = tracemalloc.take_snapshot()
+					display_top(snapshot)
 
 			i = 0
 			while i < len(rso.channels)*mult*(float(rso.sps)/100):	# way of reducing CPU load while keeping stream up to date

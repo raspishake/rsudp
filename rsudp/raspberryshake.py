a75557ec5ac303126fb12d398ae394122ff89ca1
Ian Nesbitt <ian.nesbitt@gmail.com>
2019-08-27 17:46:07 -0500

    working print and alert threads

--- rsudp/raspberryshake.py
+++ rsudp/raspberryshake.py
@@ -4,14 +4,21 @@ import datetime as dt
 import signal
 from threading import Thread
 from queue import Queue
+from obspy.core.trace import Trace
+from obspy.core.stream import Stream
+from obspy.signal.trigger import recursive_sta_lta
+import numpy as np
+from obspy import UTCDateTime
+from datetime import datetime, timedelta
 
 qsize = 120 			# max UDP queue size is 30 seconds' worth of seismic data
 queue = Queue(qsize)	# master queue
-destinations = {}		# queues to write to
+destinations = []		# queues to write to
 
 initd, sockopen = False, False
 to = 10								# timeout
 firstaddr = ''
+inv = False
 
 def printM(msg):
 	'''Prints messages with datetime stamp.'''
@@ -190,6 +197,55 @@ def getTTLCHN():
 	return len(getCHNS())
 
 
+def get_inventory(sta='Z0000'):
+	global inv
+	if 'Z0000' in sta:
+		RS.printM('No station name given, continuing without inventory.')
+		inv = False
+	else:
+		try:
+			RS.printM('Fetching inventory for station %s.%s from Raspberry Shake FDSN.' % (RS.net, RS.sta))
+			inv = read_inventory('https://fdsnws.raspberryshakedata.com/fdsnws/station/1/query?network=%s&station=%s&level=resp&format=xml'
+								 % (RS.net, RS.sta))
+			RS.printM('Inventory fetch successful.')
+		except:
+			RS.printM('Inventory fetch failed, continuing without.')
+			inv = False
+
+
+def make_trace(d):
+	'''Makes a trace and assigns it some values using a data packet.'''
+	ch = ['EHZ']#RS.getCHN(d)							# channel
+	if ch:# in channels:
+		t = getTIME(d)							# unix epoch time since 1970-01-01 00:00:00Z, or as obspy calls it, "timestamp"
+		st = getSTREAM(d)						# samples in data packet in list [] format
+		tr = Trace(data=np.ma.MaskedArray(st))		# create empty trace
+		tr.stats.network = net						# assign values
+		tr.stats.location = '00'
+		tr.stats.station = sta
+		tr.stats.channel = ch
+		tr.stats.sampling_rate = 100#sps
+		tr.stats.starttime = UTCDateTime(t)
+		if inv:
+			#try:
+			tr.attach_response(inv)
+			# except:
+			# 	RS.printM('ERROR attaching inventory response. Are you sure you set the station name correctly?')
+			# 	RS.printM('    This could indicate a mismatch in the number of data channels between the inventory and the stream.')
+			# 	RS.printM('    For example, if you are receiving RS4D data, please make sure the inventory you download has 4 channels.')
+		return tr
+
+# Then make repeated calls to this, to continue adding trace data to the stream
+def update_stream(stream, d, **kwargs):
+	'''Returns an updated trace object with new data, merged down to one trace per available channel.'''
+	while True:
+		try:
+			return stream.append(make_trace(d)).merge(**kwargs)
+			return stream
+		except TypeError:
+			pass
+
+
 class ProducerThread(Thread):
 	def run(self):
 		"""
@@ -235,4 +291,117 @@ class ConsumerThread(Thread):
 			queue.task_done()
 
 			for q in destinations:
-				destinations[q].put(p)
+				q.put(p)
+
+
+class AlertThread(Thread):
+	def __init__(self):
+		"""
+		Initialize the thread
+		"""
+		super().__init__()
+		global destinations, alertqno
+
+		alrtq = Queue(qsize)
+		destinations.append(alrtq)
+		alertqno = len(destinations) - 1
+
+	def run(self):
+		"""
+
+		"""
+		alert_stream = Stream()
+		sta='R3BCF'
+		#get_inventory(sta=sta)
+		#if inv:
+			#alert_stream.attach_response(inv)
+		alert_stream.select(component='Z')
+
+		tf = 4
+		n = 0
+		sta = 5
+		lta = 10
+
+		wait_pkts = tf * lta
+		while True:
+			while True:
+				d = destinations[alertqno].get()
+				destinations[alertqno].task_done()
+				alert_stream = update_stream(
+					stream=alert_stream, d=d, fill_value='latest')
+				df = alert_stream[0].stats.sampling_rate
+				break
+
+			if n > (lta * tf):
+				obstart = alert_stream[0].stats.endtime - timedelta(seconds=lta)	# obspy time
+				alert_stream = alert_stream.slice(starttime=obstart)	# slice the stream to the specified length (seconds variable)
+
+				cft = recursive_sta_lta(alert_stream[0], int(sta * df), int(lta * df))
+				if cft.max() > 1.5:
+				#cft = z_detect(alert_stream[0], int(sta * df))
+				#if cft.max() > 1.1:
+					printM('Event detected! CFT: %s (waiting %s sec for clear trigger)' % (cft.max(), lta))
+					n = 1
+				#else:
+				#	RS.printM('                     CFT: %s' % (cft.max()))
+			elif n == 0:
+				printM('Earthquake trigger warmup time of %s seconds...' % (lta))
+				n += 1
+			elif n == (tf * lta):
+				printM('Earthquake trigger up and running normally.')
+				n += 1
+			else:
+				n += 1
+
+
+class PlotThread(Thread):
+	def __init__(self):
+		"""
+		Initialize the thread
+		"""
+		super().__init__()
+		global destinations, plotqno
+
+		plotq = Queue(qsize)
+		destinations.append(prntq)
+		plotqno = len(destinations) - 1
+
+	def run(self):
+		"""
+
+		"""
+
+		seconds = 30
+
+		print_stream = Stream()
+		sta='R3BCF'
+		n = 0
+		while True:
+			d = destinations[plotqno].get()
+			destinations[plotqno].task_done()
+			alert_stream = update_stream(
+				stream=alert_stream, d=d, fill_value='latest')
+			df = alert_stream[0].stats.sampling_rate
+
+
+class PrintThread(Thread):
+	def __init__(self):
+		"""
+		Initialize the thread
+		"""
+		super().__init__()
+		global destinations, printqno
+
+		prntq = Queue(qsize)
+		destinations.append(prntq)
+		printqno = len(destinations) - 1
+
+	def run(self):
+		"""
+
+		"""
+
+		while True:
+			d = destinations[printqno].get()
+			destinations[printqno].task_done()
+			print(str(d))
--- rsudp/udp2obspy.py
+++ rsudp/udp2obspy.py
@@ -19,12 +19,6 @@ import rsudp.rs2o as rso
 
 inv = False
 
-global drawq, alrtq, forwq
-global destinations
-drawq = Queue(RS.qsize)	# plot queue
-alrtq = Queue(RS.qsize)	# alert queue
-forwq = Queue(RS.qsize)	# forwarding queue
-
 
 def start_listen(port, sta):
 	RS.initRSlib(dport=port, rssta=sta)
@@ -46,60 +40,9 @@ def get_inventory(sta='Z0000'):
 			inv = False
 
 
-class AlertThread(Thread):
-	def run(self):
-		"""
-
-		"""
-		global alrtq, inv
-		alert_stream = Stream()
-		sta='R3BCF'
-		get_inventory(sta=sta)
-		if inv:
-			alert_stream.attach_response(inv)
-		alert_stream.select(component='Z')
-
-		tf = 4
-		n = 0
-		sta = 5
-		lta = 10
-
-		wait_pkts = tf * lta
-		while True:
-			while True:
-				d = RS.destinations['alerts'].get()
-				RS.destinations['alerts'].task_done()
-				alert_stream = rso.update_stream(
-					stream=alert_stream, d=d, fill_value='latest')
-				df = alert_stream[0].stats.sampling_rate
-				break
-
-			if n > (lta * tf):
-				obstart = alert_stream[0].stats.endtime - timedelta(seconds=lta)	# obspy time
-				alert_stream = alert_stream.slice(starttime=obstart)	# slice the stream to the specified length (seconds variable)
-
-				cft = recursive_sta_lta(alert_stream[0], int(sta * df), int(lta * df))
-				if cft.max() > 1.4:
-				#cft = z_detect(alert_stream[0], int(sta * df))
-				#if cft.max() > 1.1:
-					RS.printM('Event detected! CFT: %s (waiting %s for clear trigger)' % (cft.max(), lta))
-					n = 1
-				#else:
-				#	RS.printM('                     CFT: %s' % (cft.max()))
-			elif n == 0:
-				RS.printM('Earthquake trigger warmup time of %s seconds...' % (lta))
-				n += 1
-			elif n == (tf * lta):
-				RS.printM('Earthquake trigger up and running normally.')
-				n += 1
-			else:
-				n += 1
-
-
-
 
 def main():
-	global alrtq
+	#global alrtq
 	port = 18001
 	start_listen(port=port, sta='R3BCF')
 	RS.test_connection()
@@ -107,16 +50,16 @@ def main():
 	prod = RS.ProducerThread()
 	cons = RS.ConsumerThread()
 
-	if True:
-		alrt = AlertThread()
-		#draw = DrawThread()
-		#forw = ForwardThread()
-		RS.destinations['alerts'] = alrtq#, drawq, forwq]
+	alrt = RS.AlertThread()
+	prnt = RS.PrintThread()
+	#plot = RS.PlotThread()
 
 	prod.start()
 	cons.start()
 
 	alrt.start()
+	prnt.start()
+	#plot.start()
 
 if __name__ == '__main__':
 	main()

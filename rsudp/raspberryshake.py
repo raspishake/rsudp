40e8ea6926dcaeb527cbad65628d6b131951f312
iannesbitt <ian.nesbitt@gmail.com>
2019-09-08 22:51:57 -0500

    fixing maximization issue in Tk

--- rsudp/raspberryshake.py
+++ rsudp/raspberryshake.py
@@ -870,7 +870,7 @@ class PlotThread(Thread):
 			if self.qt:	# try maximizing in Qt first
 				figManager.window.showMaximized()
 			else:	# if Qt fails, try Tk
-				figManager.window.state('zoomed')
+				figManager.window.showMaximized()
 		plt.draw()									# draw the canvas
 		self.fig.canvas.start_event_loop(0.005)		# wait for canvas to update
 		if self.fullscreen:		# carefully designed plot layout parameters

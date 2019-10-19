d6966b411ba2450500173181db7ac0a1d03fd085
Ian Nesbitt <ian.nesbitt@gmail.com>
2019-10-19 13:26:14 -0500

    creating update script and updating install script

--- unix-update-rsudp.sh
+++ unix-update-rsudp.sh
@@ -6,15 +6,7 @@ arch=$(uname -m)    # machine hardware
 os=$(uname -s)      # kernel name
 node=$(uname -n)    # node name
 gnu=$(uname -a | grep GNU)  # is this GNU?
-tmp="/tmp"          # temporary directory for install file
-exe="conda-install.sh" # install file name
-tmp_exe="$tmp/$exe" # install file loc/name
 conda="conda"       # anaconda executable or alias
-macos_exe="Miniconda3-4.7.12-MacOSX-x86_64.sh"
-linux_exe="Miniconda3-4.7.12-Linux-x86_64.sh"
-arm_exe="Berryconda3-2.0.0-Linux-armv7l.sh"
-x86_base_url="https://repo.anaconda.com/miniconda/"
-arm_base_url="https://github.com/jjhelmus/berryconda/releases/download/v2.0.0/"
 if [[ "$arch" == "armv"* ]]; then release='berryconda3'; else release='miniconda3'; fi
 # conda install location:
 prefix="$HOME/$release"         # $HOME/miniconda3 is default location
@@ -25,7 +17,7 @@ config="$HOME/.config/rsudp"    # config location
 settings="$config/rsudp_settings.json"  # settings file
 
 echo "--------------------------------------------"
-echo "Raspberry Shake UDP client installer $ver"
+echo "Raspberry Shake UDP client updater $ver"
 echo "Ian Nesbitt, Raspberry Shake S.A., 2019"
 echo "--------------------------------------------"
 echo "Please follow instructions in prompts."
@@ -48,7 +40,7 @@ outdir=${outdir// /_}
 outdir=${outdir//[^a-zA-Z0-9_/]/}
 # finally, lowercase with TR
 outdir=`echo -n $outdir | tr A-Z a-z`
-if [ -z "$var" ];
+if [ -z "$var" ]; then
   echo "No directory was provided, using $HOME/rsudp"
   outdir="$HOME/rsudp"
 fi
@@ -94,142 +86,28 @@ else
 fi
 
 if [ -z ${conda_exists+x} ]; then
-  echo "Cannot find conda installation; will try installing $release."
-  # get ready to install anaconda or berryconda
-  echo "Found $os environment on $arch."
-  echo "Install location: $prefix"
-  echo "Ready to download $release"
-  echo "The download could be as large as 200 MB."
-  read -n1 -rp $'Press any key to continue or Ctrl+C to exit...\n\n'
-
-  if [ ! -z ${PYTHONPATH+x} ]; then
-    # conda does not like $PYTHONPATH, and $PYTHONPATH is depreciated,
-    # so we can get away with disabling it during installation.
-    # because it is sourced, it will come back when the user opens a new shell
-    # and conda will complain about it directly to the user.
-    unset $PYTHONPATH
-  fi
-
-  if [[ "$arch" == "armv"* ]]; then
-    # installing on ARM architecture (RPi or similar)
-    rpi="rpi"
-
-    if [[ "$node" == "raspberryshake" ]]; then
-      # warn the user about installing on a Shake
-      echo '---------------------------------------------------------------'
-      echo "WARNING: You are installing this on the Raspberry Shake itself."
-      echo "Although this is possible, it is not tested or supported."
-      echo "Raspberry Shake S.A. is not liable for strange Shake behavior"
-      echo "if you choose to do this! Proceed at your own risk."
-      read -n1 -rp $'Press any key to continue or Ctrl+C to exit...\n'
-    fi
-
-    wget "$arm_base_url$arm_exe" -O "$tmp_exe" && dl=1
-
-  else
-    if [[ "$os" == "Linux" ]]; then
-      conda_installer=$linux_exe
-
-    elif [[ "$os" == "Darwin" ]]; then
-      conda_installer=$macos_exe
-
-    else
-      echo "ERROR: Script does not support this OS."
-      echo "Please install Anaconda 3 by hand from the following link:"
-      echo "https://www.anaconda.com/distribution/#download-section"
-      exit 1
-    fi
-
-    wget "$x86_base_url$conda_installer" -O "$tmp_exe" && dl=1
-  fi
-
-  if [ ! -z ${dl+x} ]; then
-    chmod +x "$tmp_exe"
-    echo "Installing $release..."
-    cd "$tmp" && ./$exe -b -p $prefix
-    echo "Cleaning up temporary files..."
-    rm "$tmp_exe"
-    echo "Updating base conda environment..."
-    $conda update conda -y
-  else
-    echo "Something went wrong downloading $release. Check the error and try again."
-    exit 2
-  fi
-
-else
-    previous_conda=1
-    echo "Anaconda installation found at $prefix"
-    echo "conda executable: $(which conda)"
+  echo "Cannot find conda installation; please try running the installer script."
+  exit 2
 fi
 
-comment="# added by rsudp/conda installer"
 sourceline=". $prefix/etc/profile.d/conda.sh"
-
 if grep -Fxq "$sourceline" "$HOME/.bashrc"; then
   echo "Source line already exists in $HOME/.bashrc"
   sourced=1
-else
-  echo "----------------------------------------------"
-  echo "The script will now append a sourcing line to your ~/.bashrc file in order to"
-  echo 'make activating conda easier in the future (just type "conda activate" into a terminal).'
-  echo "This line is: $sourceline"
-  read -rp $'Press Enter to prevent this, or type yes and press Enter to proceed...\n' key
-
-  if [[ "$key" == "yes" ]]; then
-    echo "Appending sourcing line to bashrc..."
-    echo $comment >> $HOME/.bashrc
-    echo $sourceline >> $HOME/.bashrc
-    sourced=1
-  else
-    echo "Not appending sourcing line to bashrc."
-    echo "You can add it later by adding the following line to the bottom of ~/.bashrc:"
-    echo $sourceline
-  fi
 fi
 echo "Sourcing..."
 $sourceline
 echo "Activating conda..."
 conda activate && conda_exists=1
 
-if [ -z ${conda_exists+x} ]; then
-  echo "ERROR: Anaconda install failed. Check the error output and try again."
-  exit 2
-fi
-
-if [[ "$arch" == "armv"* ]]; then
-  env_install="conda create -n rsudp python=3.6.6 numpy matplotlib future scipy lxml sqlalchemy -y"
-else
-  env_install="conda create -n rsudp python=3.7.4 matplotlib=3.1.1 numpy=1.16.4 future scipy lxml sqlalchemy obspy -y"
-fi
-
-# check for conda forge channel; if it's not there add it
-if [ ! -f $HOME/.condarc ]; then
-  echo "No $HOME/.condarc file exists. Creating..."
-  echo $'channels:\n  -\n   defaults\n  -\n   rpi\n  -\n   conda-forge\n' > $HOME/.condarc
-fi
-if [[ "$arch" == "armv"* ]]; then
-  cat $HOME/.condarc | grep "rpi" >/dev/null && echo "Found rpi channel in $HOME/.condarc" ||
-  (echo "Appending rpi to conda channels..." &&
-  conda config --append channels rpi)
-fi
-cat $HOME/.condarc | grep "conda-forge" >/dev/null && echo "Found conda-forge channel in $HOME/.condarc"  ||
-(echo "Appending conda-forge to conda channels..." &&
-conda config --append channels conda-forge)
-
 if [ -d $prefix/envs/rsudp ]; then
-  echo "Another rsudp conda environment already exists at $prefix/envs/rsudp" &&
-  echo "Do you want to use it, or remove it and install a new one?"
-  read -rp $'Press Enter to use it, or type yes and press Enter to reinstall:\n' reinstall
-  
-  if [[ "$reinstall" == "yes" ]]; then
-    echo "Removing old environment..."
-    rm -r $prefix/envs/rsudp
-    echo "Reinstalling rsudp conda environment..." &&
-    $env_install
-  fi
+  echo "A rsudp conda environment exists at $prefix/envs/rsudp" &&
+  echo "Do you want to update it?"
+  read -rp $'Press Enter to use it or Ctrl+C to quit:\n' reinstall
 else
-  echo "Creating and installing rsudp conda environment..." &&
-  $env_install
+  echo "No rsudp environment exists. Please use the installer script."
+  echo "Exiting now."
+  exit 2
 fi
 
 if [ -d $prefix/envs/rsudp ]; then
@@ -259,7 +137,6 @@ if [ ! -z ${success+x} ]; then
   echo "Failed to create settings file. Either the script could not create a folder at $config, or dumping the settings did not work." ||
   echo "If you would like, you can dump the settings to a file manually by running the command" ||
   echo "rs-client -d rsudp_settings.json"
-
 
   if [ -z ${previous_conda+x} ]; then
     if [ -z ${sourced+x} ]; then

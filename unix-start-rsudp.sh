cc1ffac2860037a7677369e79ca84a50d82b20cd
Ian Nesbitt <ian.nesbitt@gmail.com>
2019-10-22 15:56:48 -0500

    adding start script

---
+++ unix-start-rsudp.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# settings file (change this to start with a different settings file)
+settings="$HOME/.config/rsudp/rsudp_settings.json"
+
+# other necessary stuff
+if [[ "$arch" == "armv"* ]]; then release='berryconda3'; else release='miniconda3'; fi
+# conda install location:
+prefix="$HOME/$release"         # $HOME/miniconda3 is default location
+full="$HOME/anaconda3"          # full release install location
+
+if [ -f "$prefix/etc/profile.d/conda.sh" ]; then
+  . $prefix/etc/profile.d/conda.sh
+elif [ -f "$full/etc/profile.d/conda.sh" ]; then
+  . $full/etc/profile.d/conda.sh
+else
+  echo "Could not find an anaconda installation. Have you used the installer script to install rsudp, or is anaconda in a nonstandard location?"
+  exit 2
+fi
+conda activate rsudp
+echo "Installing from the git directory..."
+pip install $( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd ) &>/tmp/rsudp/rsudp.log
+echo "Done."
+rs-client -s $settings

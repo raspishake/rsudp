f4a2941fe9c089e6bc35904a3de1089b6eabf0dc
iannesbitt <ian.nesbitt@gmail.com>
2020-03-19 01:06:43 -0500

    reducing complexity in `Producer.run()`

--- rsudp/p_producer.py
+++ rsudp/p_producer.py
@@ -34,6 +34,51 @@ class Producer(Thread):
 
 		printM('Starting.', self.sender)
 
+
+	def _filter_sender(self, addr, data):
+		'''
+		Filter the message sender and put data on the consumer queue.
+		'''
+		if self.firstaddr == '':
+			self.firstaddr = addr[0]
+			printM('Receiving UDP data from %s' % (self.firstaddr), self.sender)
+		if (self.firstaddr != '') and (addr[0] == self.firstaddr):
+			self.queue.put(data)
+			if data.decode('utf-8') == 'TERM':
+				RS.producer = False
+				self.stop = True
+		else:
+			if addr[0] not in self.blocked:
+				printM('Another IP (%s) is sending UDP data to this port. Ignoring...'
+						% (addr[0]), self.sender)
+				self.blocked.append(addr[0])
+
+
+	def _tasks(self):
+		'''
+		Execute tasks based on the states of sub-consumers.
+		'''
+		for thread in self.threads:
+			# for each thread here
+			if thread.alarm:
+				# if there is an alarm in a sub thread, send the ALARM message to the queues
+				self.queue.put(RS.msg_alarm(thread.alarm))
+				printM('%s thread has indicated alarm state, sending ALARM message to queues'
+						% thread.sender, sender=self.sender)
+				# now re-arm the trigger
+				thread.alarm = False
+			if thread.alarm_reset:
+				# if there's an alarm_reset flag in a sub thread, send a RESET message
+				self.queue.put(RS.msg_reset(thread.alarm_reset))
+				printM('%s thread has indicated alarm reset, sending RESET message to queues'
+						% thread.sender, sender=self.sender)
+				# re-arm the trigger
+				thread.alarm_reset = False
+			if not thread.alive:
+				# if a thread stops, set the stop flag
+				self.stop = True
+
+
 	def run(self):
 		"""
 		Distributes queue objects to execute various other tasks: for example,
@@ -42,33 +87,8 @@ class Producer(Thread):
 		"""
 		RS.producer = True
 		while RS.producer:
-			data, addr = RS.sock.recvfrom(4096)
-			if self.firstaddr == '':
-				self.firstaddr = addr[0]
-				printM('Receiving UDP data from %s' % (self.firstaddr), self.sender)
-			if (self.firstaddr != '') and (addr[0] == self.firstaddr):
-				self.queue.put(data)
-				if data.decode('utf-8') == 'TERM':
-					RS.producer = False
-					self.stop = True
-			else:
-				if addr[0] not in self.blocked:
-					printM('Another IP (%s) is sending UDP data to this port. Ignoring...'
-							% (addr[0]), self.sender)
-					self.blocked.append(addr[0])
-			for thread in self.threads:
-				if thread.alarm:
-					self.queue.put(RS.msg_alarm(thread.alarm))
-					printM('%s thread has indicated alarm state, sending ALARM message to queues'
-						   % thread.sender, sender=self.sender)
-					thread.alarm = False
-				if thread.alarm_reset:
-					self.queue.put(RS.msg_reset(thread.alarm_reset))
-					printM('%s thread has indicated alarm reset, sending RESET message to queues'
-						   % thread.sender, sender=self.sender)
-					thread.alarm_reset = False
-				if not thread.alive:
-					self.stop = True
+			self._filter_sender(RS.sock.recvfrom(4096))
+			self._tasks()
 			if self.stop:
 				RS.producer = False
 				break

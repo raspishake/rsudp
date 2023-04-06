59ee0df1fb1bf67f2fbf1e88c4dd0a25010f3609
Ian Nesbitt <ian.nesbitt@gmail.com>
2023-04-05 18:58:43 -0700

    pinning telegram version - patch for #53

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     include_package_data=True,
     install_requires=['obspy', 'numpy', 'matplotlib<3.2', 'pydub', 'twython',
-                      'python-telegram-bot'],
+                      'python-telegram-bot<=13.11'],
     entry_points = {
         'console_scripts': [
             'rs-packetloss=rsudp.packetloss:main',
